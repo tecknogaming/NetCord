140a10a0d50a30e615f1c71db0169b73350dc2e7
tecknogaming <68946687+tecknogaming@users.noreply.github.com>
2021-12-18 14:55:50 +0700

    update ready.js

--- src/events/client/ready.js
+++ src/events/client/ready.js
@@ -1,1 +1,11 @@
+const func = require("../../Utils/fuctions.js");
+const { presence } = require("../../Config/mainCfg.json");
 
+module.exports = client => {
+	func.setStatus(client, {
+		name: presence.name,
+		type: presence.type
+	})
+	console.log("ready".brightGreen)
+	console.log(func.genId(3))
+}
