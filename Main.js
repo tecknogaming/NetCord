831807f4e06a1b288bcdb6565759d4506a437266
tecknogaming <68946687+tecknogaming@users.noreply.github.com>
2021-12-18 14:20:08 +0700

    update Main.js

--- Main.js
+++ Main.js
@@ -1,1 +1,41 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const mainCfg = require("./src/config/mainCfg.json");
+const loadevents = require("./src/Utils/Handlers/events.js");
+const loadCmd = require("./src/utils/Handlers/loadCmd.js");
+const colors = require("colors");
 
+const client = new Discord.Client({
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+    intents: [ 
+        Discord.Intents.FLAGS.GUILDS,
+        Discord.Intents.FLAGS.GUILD_MEMBERS,
+        //Discord.Intents.FLAGS.GUILD_BANS,
+        //Discord.Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS,
+        //Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
+        //Discord.Intents.FLAGS.GUILD_WEBHOOKS,
+        //Discord.Intents.FLAGS.GUILD_INVITES,
+        //Discord.Intents.FLAGS.GUILD_VOICE_STATES,
+        //Discord.Intents.FLAGS.GUILD_PRESENCES,
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        //Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        //Discord.Intents.FLAGS.GUILD_MESSAGE_TYPING,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGES,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGE_REACTIONS,
+        //Discord.Intents.FLAGS.DIRECT_MESSAGE_TYPING
+    ],
+    presence: {
+      activity: {
+        name: mainCfg.presence.name, 
+        type: mainCfg.presence.type, 
+      },
+      status: mainCfg.presence.status
+    }
+});
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+loadCmd(client)
+loadevents(client)
+
+client.login(process.env.TOKEN)
