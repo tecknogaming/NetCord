3b2cc3b0755206f0a862bef5b458f4c51e930bfa
tecknogaming <68946687+tecknogaming@users.noreply.github.com>
2021-12-25 18:44:51 +0700

    update antiCrash.js

--- src/Utils/Handlers/antiCrash.js
+++ src/Utils/Handlers/antiCrash.js
@@ -1,1 +1,26 @@
+const { Log4me } = require("../log4me.js");
 
+module.exports = client => {
+	const {log} = new Log4me("antiCrash", "./logs.txt");
+	
+	process.on('unhandledRejection', (reason, p) => {
+		console.log(' [antiCrash] :: Unhandled Rejection/Catch');
+		console.log(reason, p);
+		log(reason)
+	});
+	process.on("uncaughtException", (err, origin) => {
+		console.log(' [antiCrash] :: Uncaught Exception/Catch');
+		console.log(err, origin);
+		log(err + origin)
+	})
+	process.on('uncaughtExceptionMonitor', (err, origin) => {
+		console.log(' [antiCrash] :: Uncaught Exception/Catch (MONITOR)');
+		console.log(err, origin);
+		log(err + origin)
+	});
+	process.on('multipleResolves', (type, promise, reason) => {
+		console.log(' [antiCrash] :: Multiple Resolves');
+		console.log(type, promise, reason);
+		log(`${type}: ${promise} ${reason}`)
+	});
+}
